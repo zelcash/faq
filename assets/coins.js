be0779a3b1d593481d17ea45491a14f028769b60
Lumiboy <tha_lumi@hotmail.com>
2019-01-25 04:25:41 +0100

    Fetch Price and Popup info window

---
+++ assets/coins.js
@@ -0,0 +1,57 @@
+var data = 
+{
+    "coins" : [
+        {
+            "coin" : "Bitcoin",
+            "ticker" : "BTC",
+            "logo" : "logos/Bitcoin.svg",
+            "status" : "Online",
+            "price" : "1",
+            "explorer" : "#",
+            "website" : "https://bitcoin.org",
+            "twitter" : "twitter",
+            "discord" : null,
+            "telegram" : null,
+            "info" : "Bitcoin is the first"
+        },
+        {
+            "coin" : "ZelCash",
+            "ticker" : "Zel",
+            "logo" : "logos/Zelcash.svg",
+            "status" : "Online",
+            "price" : "1",
+            "explorer" : "explorer",
+            "website" : "https://zel.cash",
+            "twitter" : "twitter",
+            "discord" : "discord",
+            "telegram" : "telegram",
+            "info" : "ZelCash is the one"
+        },
+        {
+            "coin" : "Ethereum",
+            "ticker" : "ETC",
+            "logo" : "logos/Ethereum.svg",
+            "status" : "Offline",
+            "price" : "1",
+            "explorer" : "#",
+            "website" : "#",
+            "twitter" : "#",
+            "discord" : "#",
+            "telegram" : "#",
+            "info" : "Ethereum is done"
+        },
+        {
+            "coin" : "Litecoin",
+            "ticker" : "LTC",
+            "logo" : "logos/Litecoin.svg",
+            "status" : "Update",
+            "price" : "1",
+            "explorer" : "#",
+            "website" : "#",
+            "twitter" : "#",
+            "discord" : null,
+            "telegram" : null,
+            "info" : "Litecoin is the cockroch"
+        }
+    ]
+}
