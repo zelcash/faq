4a1ae060f704252b43a46616f273435b6e90dba0
Scribbles-MCAMK <miles@mksystems.co>
2019-01-27 03:56:47 +0200

    New logos and added zec

--- assets/coins.js
+++ assets/coins.js
@@ -10,7 +10,7 @@ var data =
             "twitter" : "twitter",
             "discord" : null,
             "telegram" : null,
-            "info" : "Bitcoin is the first"
+            "info" : "Bitcoin is an innovative payment network and a new kind of money. Find all you need to know and get started with Bitcoin on bitcoin.org."
         },
         {
             "coin" : "ZelCash",
@@ -18,10 +18,10 @@ var data =
             "logo" : "logos/Zelcash.svg",
             "explorer" : "https://explorer.zel.zelcore.io",
             "website" : "https://zel.cash",
-            "twitter" : "twitter",
-            "discord" : "discord",
-            "telegram" : "telegram",
-            "info" : "ZelCash is the one"
+            "twitter" : "https://twitter.com/zelcash",
+            "discord" : "https://discord.io/zelcash",
+            "telegram" : "https://t.me/zelcash",
+            "info" : "Zelcash is a cryptocurrency that holds a central role in the Zel Ecosystem. It gives the incentive for ZelNode ownership to enable the creation of ZelDex and the ZelDev Platform, enabling us to create a truly decentralized development platform."
         },
         {
             "coin" : "Ethereum",
@@ -29,10 +29,10 @@ var data =
             "logo" : "logos/Ethereum.svg",
             "explorer" : "https://explorer.eth.zelcore.io",
             "website" : "https://www.ethereum.org",
-            "twitter" : "#",
-            "discord" : "#",
-            "telegram" : "#",
-            "info" : "Ethereum is done"
+            "twitter" : "https://twitter.com/ethereum",
+            "discord" : null,
+            "telegram" : null,
+            "info" : "Ethereum is a decentralized platform for applications that run exactly as programmed without any chance of fraud, censorship or third-party interference."
         },
         {
             "coin" : "Litecoin",
@@ -43,7 +43,18 @@ var data =
             "twitter" : "#",
             "discord" : null,
             "telegram" : null,
-            "info" : "Litecoin is the cockroach"
+            "info" : "Litecoin is a cryptocurrency that enables instant payments to anyone in the world and that can be efficiently mined with consumer-grade hardware."
+        },
+        {
+            "coin" : "Zcash",
+            "ticker" : "zec",
+            "logo" : "logos/Zcash.svg",
+            "explorer" : "https://explorer.zec.zelcore.io",
+            "website" : "https://z.cash",
+            "twitter" : "https://twitter.com/zcashco",
+            "discord" : null,
+            "telegram" : null,
+            "info" : "Zcash is a digital currency with strong privacy features. Transact efficiently and safely, with low fees, while ensuring digital transactions remain private."
         }
     ]
 }
